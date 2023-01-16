65184f22f379eea9b1342114e85a4e55d642eb2f
Bow Jaro <21520932@gm.uit.edu.vn>
2023-01-16 20:30:36 +0700

    Home button

--- Mp3/MP3_Final/MainWindow.xaml.cs
+++ Mp3/MP3_Final/MainWindow.xaml.cs
@@ -559,6 +559,7 @@ namespace MP3_Final
 
         private void Favorite_Click(object sender, RoutedEventArgs e)
         {
+            NonePlaylist();
             onSearch = false;
             playSearch = false;
             StreamReader reader = new StreamReader(fav);
@@ -617,7 +618,24 @@ namespace MP3_Final
             System.Windows.Controls.Button button = sender as System.Windows.Controls.Button;
             string path = head + button.Content.ToString() + tail;
             currentPlaylist = button.Tag.ToString();
+            ChangeColorClickPlayList();
             OpenPlayList(path);
+        }
+
+        void ChangeColorClickPlayList()
+        {
+            var list = listMenu.Children;
+            foreach (var item in list)
+            {
+                System.Windows.Controls.Button button = item as System.Windows.Controls.Button;
+                button.Background = (SolidColorBrush)new BrushConverter().ConvertFrom("#02be68");
+                button.Foreground = Brushes.Black;
+                if (button.Tag == currentPlaylist)
+                {
+                    button.Background = (SolidColorBrush)new BrushConverter().ConvertFrom("#30ad9a");
+                    button.Foreground = Brushes.WhiteSmoke;
+                }
+            }
         }
 
         void OpenPlayList(string path)
@@ -878,6 +896,7 @@ namespace MP3_Final
         //real load file
         private void LoadFileButton(object sender, RoutedEventArgs e)
         {
+            NonePlaylist();
             if (onSearch)
             {
                 songMenu.Children.Clear();
@@ -926,6 +945,7 @@ namespace MP3_Final
         //real load folder
         private void LoadFolderButton(object sender, RoutedEventArgs e)
         {
+            NonePlaylist();
             if (onSearch)
             {
                 songMenu.Children.Clear();
@@ -1077,6 +1097,25 @@ namespace MP3_Final
             for (int i = 0; i < search.Count; i++)
             {
                 Add_UcSongName(search[i], i);
+            }
+        }
+
+        private void Home_Click(object sender, RoutedEventArgs e)
+        {
+            NonePlaylist();
+            onSearch = false;
+            playSearch = false;
+            OpenPlayList(head + @"History" + tail);
+        }
+
+        void NonePlaylist()
+        {
+            var list = listMenu.Children;
+            foreach (var item in list)
+            {
+                System.Windows.Controls.Button button = item as System.Windows.Controls.Button;
+                button.Background = (SolidColorBrush)new BrushConverter().ConvertFrom("#02be68");
+                button.Foreground = Brushes.Black;
             }
         }
 
